63657dcae4f52b42a114438462484f32d15313d7
Pupppppxz <69138763+Pupppppxz@users.noreply.github.com>
2020-09-12 16:25:42 +0700

    Update Source.cpp

--- sort_number/Project1/Source.cpp
+++ sort_number/Project1/Source.cpp
@@ -9,7 +9,7 @@ int main()
 {
 	int number[6];
 	int i;
-	printf("Enter 6 integer number");
+	printf("Enter 6 integer number\n");
 	for (i = 0; i < 6; i++) 
 	{
 		printf("Enter the positive integer [%d] : ",i+1);
